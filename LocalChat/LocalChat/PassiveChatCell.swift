fc824bfbaa906b67a38a7ab0c15eb8fc5a61e774
hasanmainul <mainul.haque@framgia.com>
2016-05-10 08:52:43 +0600

    Home Screen Design

---
+++ LocalChat/LocalChat/PassiveChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  PassiveChatCell.swift
+//  LocalChat
+//
+//  Created by Md Mainul Haque on 5/9/16.
+//  Copyright © 2016 Md Mainul Haque. All rights reserved.
+//
+
+import UIKit
+
+class PassiveChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
